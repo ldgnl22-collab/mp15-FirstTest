321f0fd16ba346458cf3880f37118dc4f017c9f5
Rocket22 <ldgnl22@gmail.com>
2026-08-28 14:24:53 +0900

    [Feat] 키오스크클래스에 할인 메세지 적용

--- 260828_mp15_FirstTest/260828_mp15_FirstTest/Kiosk.cs
+++ 260828_mp15_FirstTest/260828_mp15_FirstTest/Kiosk.cs
@@ -5,13 +5,21 @@ public class Kiosk<T> where T : Menu
 {
     // 리스트에 메뉴 추가
     private List<T> list = new List<T>();
-    private int count = 0;
+    private int count = 1;
 
     public void AddMenuList(T menu)
     {
         list.Add(menu);
         menu.LotNumber = count;
-        Console.WriteLine($"{menu.LotNumber}{menu.Name}");
+        Console.Write($"{menu.LotNumber}. {menu.Name} ({menu.Type})  가격: {menu.Price}");
+        if (menu is IDiscount)
+        {
+            Console.WriteLine($"  [{(menu as IDiscount).DiscountMessage()}]");
+        }
+        else
+        {
+            Console.WriteLine($"  [{menu.Discount}]");
+        }
         count++;
     }
 }
