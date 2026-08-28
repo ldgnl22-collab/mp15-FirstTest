375011cd2e749894bcb6506a97e2bdd03ab03620
Rocket22 <ldgnl22@gmail.com>
2026-08-28 12:13:52 +0900

    [Feat] Kiosk 클래스 추가

---
+++ 260828_mp15_FirstTest/260828_mp15_FirstTest/Kiosk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kiosk<T> where T : Menu
+{
+    // 리스트에 메뉴 추가
+    private List<T> list = new List<T>();
+    private int lotCount = 0;
+
+    public void AddMenu(Menu menu)
+    {
+        menu.LotNumber = lotCount;
+        Console.WriteLine($"{menu.Name}");
+        lotCount++;
+    }
+}
