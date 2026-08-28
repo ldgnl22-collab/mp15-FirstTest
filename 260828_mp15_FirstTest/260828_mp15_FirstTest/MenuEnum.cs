d6d957b44dd49a6f87fcb2263f384dd55547e301
Rocket22 <ldgnl22@gmail.com>
2026-08-28 11:19:54 +0900

    [Feat] 메뉴 Enum 추가

---
+++ 260828_mp15_FirstTest/260828_mp15_FirstTest/MenuEnum.cs
@@ -0,0 +1,19 @@
+﻿
+
+public enum DrinkList
+{
+    Americano,
+    IceTea,
+    MixCoffee
+}
+public enum DessertList
+{
+    CheeseCake,
+    Madeleine,
+    ButterCookie
+}
+public enum BreadList
+{
+    PizzaBread,
+    MorningBread
+}
