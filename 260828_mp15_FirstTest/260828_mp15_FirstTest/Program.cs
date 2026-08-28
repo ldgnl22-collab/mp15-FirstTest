dcdd510ba89de7be7ab9cbe508ccdb7875048475
Rocket22 <ldgnl22@gmail.com>
2026-08-28 13:26:51 +0900

    [Feat] kiosk에 메뉴 추가 구현

--- 260828_mp15_FirstTest/260828_mp15_FirstTest/Program.cs
+++ 260828_mp15_FirstTest/260828_mp15_FirstTest/Program.cs
@@ -11,7 +11,13 @@ class Program
 
         Kiosk<Menu> kiosk = new Kiosk<Menu>();
         
-        
+        kiosk.AddMenuList(new Drink("아메리카노", 2300));
+        kiosk.AddMenuList(new Drink("아이스 티", 2000));
+        kiosk.AddMenuList(new Drink("아이스 라떼", 2700));
+        kiosk.AddMenuList(new Dessert("두쫀쿠", 3000));
+        kiosk.AddMenuList(new Dessert("마들렌", 2000));
+        kiosk.AddMenuList(new Bread("피자 빵", 2500));
+        kiosk.AddMenuList(new Bread("모닝 빵", 1700));
         
     }
 }
