0027b6daadce0957529659dbe90a1e252c91aa52
Rocket22 <ldgnl22@gmail.com>
2026-08-28 12:57:14 +0900

    [Test] 테스트 코드

--- 260828_mp15_FirstTest/260828_mp15_FirstTest/Program.cs
+++ 260828_mp15_FirstTest/260828_mp15_FirstTest/Program.cs
@@ -7,6 +7,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<Drink> drinkList = new();
+        List<Dessert> dessertList = new();
+        List<Bread> breadList = new();
         
+        drinkList.Add(new Drink("Americano", 2000));
+        
+        // 메뉴 상속받는 것들을 리스트로 담는 클래스
+
+        Kiosk<Menu> kiosk = new Kiosk<Menu>();
+        
+        kiosk.AddMenu(drinkList[0]);
     }
 }
