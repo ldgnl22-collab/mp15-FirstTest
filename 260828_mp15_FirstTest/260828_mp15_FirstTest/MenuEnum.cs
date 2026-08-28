04b848aecbfdfa644b3da6d77cc9b7e3a800ca19
Rocket22 <ldgnl22@gmail.com>
2026-08-28 12:55:32 +0900

    [Fix] 종류별 Enum 제거

--- 260828_mp15_FirstTest/260828_mp15_FirstTest/MenuEnum.cs
+++ 260828_mp15_FirstTest/260828_mp15_FirstTest/MenuEnum.cs
@@ -6,21 +6,3 @@ public enum MenuType
     Dessert,
     Bread
 }
-
-public enum DrinkList
-{
-    Americano,
-    IceTea,
-    MixCoffee
-}
-public enum DessertList
-{
-    CheeseCake,
-    Madeleine,
-    ButterCookie
-}
-public enum BreadList
-{
-    PizzaBread,
-    MorningBread
-}
