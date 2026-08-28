a41ee7fa60cb92ad6b729b53d16465d5e34f77d1
Rocket22 <ldgnl22@gmail.com>
2026-08-28 13:38:11 +0900

    [Feat] 할인 품목 인터페이스 적용

--- 260828_mp15_FirstTest/260828_mp15_FirstTest/Bread.cs
+++ 260828_mp15_FirstTest/260828_mp15_FirstTest/Bread.cs
@@ -1,11 +1,16 @@
 ﻿
 
-public class Bread : Menu
+public class Bread : Menu, IDiscount
 {
     public Bread(string name, int price)
     {
         Name = name;
         Price = price;
         Type = "빵";
+    }
+
+    public void Discount()
+    {
+        // 3개 사면 10프로 할인
     }
 }
--- 260828_mp15_FirstTest/260828_mp15_FirstTest/Dessert.cs
+++ 260828_mp15_FirstTest/260828_mp15_FirstTest/Dessert.cs
@@ -1,11 +1,18 @@
 ﻿
 
-public class Dessert : Menu
+public class Dessert : Menu, IDiscount
 {
     public Dessert(string name, int price)
     {
         Name = name;
         Price = price;
         Type = "디저트";
+    }
+
+    public void Discount()
+    {
+        // 2개 사면 반값
+        
+        
     }
 }
