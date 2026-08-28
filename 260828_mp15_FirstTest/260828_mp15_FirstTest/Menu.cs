66c59fe1da08dfce41ef1882535b3c6d2c57fd0a
Rocket22 <ldgnl22@gmail.com>
2026-08-28 10:22:04 +0900

    [Feat] Menu 클래스에 프로퍼티 추가

--- 260828_mp15_FirstTest/260828_mp15_FirstTest/Menu.cs
+++ 260828_mp15_FirstTest/260828_mp15_FirstTest/Menu.cs
@@ -4,6 +4,9 @@ public abstract class Menu
 {
     private string name;
     private int price;
+    
+    public string Name { get; protected set; }
+    public int Price { get; protected set; }
 
     public abstract void Sell();
 }
