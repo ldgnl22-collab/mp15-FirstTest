9299a0c68688a9893b0bc99f8389659371bc5b56
Rocket22 <ldgnl22@gmail.com>
2026-08-28 16:52:32 +0900

    [Feat] 메인함수에서 테스트

--- 260828_mp15_FirstTest/260828_mp15_FirstTest/Program.cs
+++ 260828_mp15_FirstTest/260828_mp15_FirstTest/Program.cs
@@ -7,17 +7,53 @@ class Program
 {
     static void Main(string[] args)
     {
+        int selectCategory = 0;
+        int selectMenu = 0;
+
+        bool openMart = true;
+        
         // 메뉴 상속받는 것들을 리스트로 담는 클래스
 
-        Kiosk<Menu> kiosk = new Kiosk<Menu>();
-        
-        kiosk.AddMenuList(new Drink("아메리카노", 2300));
-        kiosk.AddMenuList(new Drink("아이스 티", 2000));
-        kiosk.AddMenuList(new Drink("아이스 라떼", 2700));
-        kiosk.AddMenuList(new Dessert("두쫀쿠", 3000));
-        kiosk.AddMenuList(new Dessert("마들렌", 2000));
-        kiosk.AddMenuList(new Bread("피자 빵", 2500));
-        kiosk.AddMenuList(new Bread("모닝 빵", 1700));
-        
+        Kiosk kiosk = new Kiosk();
+        ShoppingCart cart = new ShoppingCart();
+
+        while (openMart)
+        {
+            Console.Clear();
+            
+            Console.WriteLine("[메뉴판]");
+            kiosk.AddMenuList(new Drink("아메리카노", 2300));
+            kiosk.AddMenuList(new Drink("아이스 티", 2000));
+            kiosk.AddMenuList(new Drink("아이스 라떼", 2700));
+            kiosk.AddMenuList(new Dessert("두쫀쿠", 3000));
+            kiosk.AddMenuList(new Dessert("마들렌", 2000));
+            kiosk.AddMenuList(new Bread("피자 빵", 2500));
+            kiosk.AddMenuList(new Bread("모닝 빵", 1700));
+            Console.WriteLine("--------------------------------------");
+            
+            Console.WriteLine();
+            
+            Console.WriteLine("1. 담기   2. 전체 비우기   3. 결제   4. 영업종료");
+            selectCategory = ConsoleInput.ReadIntInRange("번호 입력 : ", 1, 4);
+            selectMenu = ConsoleInput.ReadIntInRange("메뉴 선택 : ", 0, kiosk.Count-1);
+
+            switch (selectCategory)
+            {
+                case 1:
+                    cart.PrintShoppingList();
+                    cart.GetMenuList(kiosk.GetMenuList[selectMenu-1]);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    openMart = false;
+                    break;
+            }
+            
+            ConsoleInput.Pause();
+            // kiosk.GetMenuList.Clear();
+        }
     }
 }
